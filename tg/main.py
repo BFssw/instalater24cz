3a52a9819b1fb9e155579d832aa9594ba2ed22b2
Baikal <37935787+BFssw@users.noreply.github.com>
2022-05-08 16:46:24 -0700

    5

--- tg/main.py
+++ tg/main.py
@@ -5,34 +5,31 @@ import codecs
 import secrets
 app = Flask(__name__)
 import subprocess
+tb = telebot.TeleBot("5306701764:AAEYRWHeiLllJHVYbgi6fRFRMEUUQ5_8q9o")
+
+
+
 
 @app.route('/bot-tg-photo',methods=['GET','POST'])
 def head():
     js = request.get_json()
-    tb = telebot.TeleBot("5306701764:AAEYRWHeiLllJHVYbgi6fRFRMEUUQ5_8q9o")
+    def dw_cn_ms(pth):
+        h=tb.download_file(pth)
+        name=secrets.token_urlsafe(10)
+        f=codecs.open(f"{name}.{pth.split('.')[-1]}",'wb')
+        f.write(h)
+        f.close()
+        subprocess.run(f"cwebp {name}.{pth.split('.')[-1]} -o {name}.webp",shell=True)
+        tb.send_message(js['message']['chat']['id'],f"https://raw.githubusercontent.com/BFssw/instalater24cz/main/tg/{name}.webp")
     try:
         if js['message']['text'] == '/start' or js['message']['text'] == '/status':
             tb.send_message(js['message']['chat']['id'],f"{js['message']['chat']['id']} - work")
     except:
-        try:
-            pth=tb.get_file(js['message']['document']['file_id']).file_path
-            h=tb.download_file(pth)
-            name=secrets.token_urlsafe(10)
-            f=codecs.open(f"{name}.{pth.split('.')[-1]}",'wb')
-            f.write(h)
-            f.close()
-            subprocess.run(f"cwebp {name}.{pth.split('.')[-1]} -o {name}.webp",shell=True)
-        except:
-            try:
-                pth=tb.get_file(js['message']['photo'][-1]['file_id']).file_path
-                h=tb.download_file(pth)
-                name=secrets.token_urlsafe(10)
-                f=codecs.open(f"{name}.{pth.split('.')[-1]}",'wb')
-                f.write(h)
-                f.close()
-                subprocess.run(f"cwebp {name}.{pth.split('.')[-1]} -o {name}.webp",shell=True)
-            except:
-                pass
+        pass
+    if 'document' in js['message']:
+        dw_cn_ms(tb.get_file(js['message']['document']['file_id']).file_path)
+    elif 'photo' in js['message']:
+        dw_cn_ms(tb.get_file(js['message']['photo'][-1]['file_id']).file_path)
     return "OK"
 
 if __name__ == '__main__':
